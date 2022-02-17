f1d8cd737ddbd3e16be754553f35460ac71577b3
Olegs Osipovs <oleg@Olegss-MacBook-Pro.local>
2022-02-17 22:08:27 +0200

    Login/out functionality updated

--- post/views.py
+++ post/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
 
+@login_required()
 def home_page(request):
     return render(request, 'post/home.html')
 
+@login_required()
 def account_page(request):
     return render(request, 'post/account.html')
 
