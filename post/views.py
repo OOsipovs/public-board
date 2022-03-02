d48fe82bc88c96ad4b3a84f6e73b14335d4adbec
Olegs Osipovs <oleg@Olegss-MacBook-Pro.local>
2022-03-02 22:29:55 +0200

    Post functionality provided

--- post/views.py
+++ post/views.py
@@ -1,13 +1,27 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 from .models import PostIt
+from .forms import PostItForm
 
 @login_required()
 def home_page(request):
+
+    if request.method == "POST":
+        form = PostItForm(request.POST)
+        form.instance.posted_by = request.user
+
+        if form.is_valid():
+            form.save()
+            return redirect('post-home')
+    else:
+        form = PostItForm()
+
+
     number_of_posts = PostIt.objects.filter(posted_by=request.user.id)
     context = {
-        'post_its': PostIt.objects.all(),
-        'number_of_personal_posts': len(number_of_posts)
+        'post_its': PostIt.objects.order_by('-posted_date'),
+        'number_of_personal_posts': len(number_of_posts),
+        'form': form
     }
 
     return render(request, 'post/home.html', context)
